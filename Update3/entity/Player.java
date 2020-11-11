4abde73ecb2ac65e2bd5c8f124bb16e7fa464867
Sutriptim Nath <65015707+Bill-ins@users.noreply.github.com>
2020-11-11 23:10:28 +0530

    Add files via upload

---
+++ Update3/entity/Player.java
@@ -0,0 +1,49 @@
+package forpractice.entity;
+
+import forpractice.Assets;
+import forpractice.Game;
+
+import java.awt.*;
+
+public class Player extends Creature{
+
+    Game game;
+    public Player(Game game, float x, float y) {
+        super(x, y, defaultCreatureWidth, defaultCreatureHeight);
+        this.game = game;
+    }
+
+    @Override
+    public void tick() {
+        getInput();
+        move();
+    }
+
+    public void getInput() {
+        xDirection = 0;
+        yDirection = 0;
+
+        if(game.getKeyManager().up) {
+            yDirection = -movement;
+        }
+
+        else if(game.getKeyManager().down) {
+            yDirection = +movement;
+        }
+
+        else if(game.getKeyManager().right) {
+            xDirection = +movement;
+        }
+
+        else if(game.getKeyManager().left) {
+            xDirection = -movement;
+        }
+
+    }
+
+    @Override
+    public void render(Graphics graphics) {
+        graphics.drawImage(Assets.rock,(int)x, (int)y, width, height,null);
+    }
+
+}
